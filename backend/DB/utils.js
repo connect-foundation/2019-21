202c1c2bc74b38851b9e85b25ae3e30efa20846a
Yujun Kim <wnsqlehlswk@naver.com>
2019-11-13 14:58:18 +0900

    lint: fix lint backend/*

--- backend/DB/utils.js
+++ backend/DB/utils.js
@@ -1,22 +1,22 @@
-import { Sequelize } from "sequelize";
+import {Sequelize} from "sequelize";
 
 function applyAllConstraint(queryInterface, tableName, constraints) {
-    constraints.forEach(({ attributes, options }) => {
-        queryInterface.addConstraint(tableName, attributes, options);
-    });
+	constraints.forEach(({attributes, options}) => {
+		queryInterface.addConstraint(tableName, attributes, options);
+	});
 }
 
 async function loadSequelize(config) {
-    const sequelize = new Sequelize(
-        config.scheme,
-        config.user,
-        config.password,
-        config
-    );
+	const sequelize = new Sequelize(
+		config.scheme,
+		config.user,
+		config.password,
+		config,
+	);
 
-    await sequelize.authenticate();
+	await sequelize.authenticate();
 
-    return sequelize;
+	return sequelize;
 }
 
-module.exports = { applyAllConstraint, loadSequelize };
+module.exports = {applyAllConstraint, loadSequelize};
