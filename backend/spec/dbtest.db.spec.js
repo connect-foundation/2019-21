765a998df8c40d97230249bc7e69e277667c3a51
Tei <teihong93@gmail.com>
2019-11-14 18:40:41 +0900

    feat: DB TESTCODE 작성 #102
    작성된 sequelize 쿼리가 eventCode 로 부터
    정상적인 question 목록을 받아오는지
    검사하는 테스트 코드를 작성함.

--- backend/spec/dbtest.db.spec.js
+++ backend/spec/dbtest.db.spec.js
@@ -1,27 +1,53 @@
 const should = require("should");
 const httpMocks = require("node-mocks-http");
-
+const Sequelize = require("sequelize");
+const {getQuestionsInEvent} = require("../DB/queries/event.js");
 const req = httpMocks.createRequest();
 const res = httpMocks.createResponse();
 const models = require("../DB/models");
+
 // middleware 테스트 해야 한다면 아래와 같이 mockhttp 사용
 // const test1 = await SOME_MIDDLE_WARE.index(req, res);
 
 describe("DB_TEST", () => {
-	it("dbtest1", async () => {
-		// 여기에 테스트 쿼리 문을 작성하고 그 값을 확인하면 됩니다.
-		const events = await models.Event.findOne({
-			attributes: ["code", "moderationOption", "replyOption"],
-		});
+	it("get question Id from EventCode k9me", async () => {
+		const questions = getQuestionsInEvent("k9me");
+		const parsedQuestion = JSON.parse(JSON.stringify(questions));
+		const compare = [];
 
-		events.dataValues.should.be.eql({
-			code: "k9me",
-			moderationOption: true,
-			replyOption: false,
-		});
+		parsedQuestion[0].Questions.forEach(elem => compare.push(elem.id));
+		compare.should.be.eql([]);
 	});
-	// 같은 방식으로 새로운 테스트를 시작할 수 있습니다.
-	it("dbtest2", async () => {
-		const newTest = "blahblah";
+	it("get question Id from EventCode u0xn", async () => {
+		const questions = getQuestionsInEvent("u0xn");
+		const parsedQuestion = JSON.parse(JSON.stringify(questions));
+		const compare = [];
+
+		parsedQuestion[0].Questions.forEach(elem => compare.push(elem.id));
+		compare.should.be.eql([119]);
+	});
+	it("get question Id from EventCode 1cfs", async () => {
+		const questions = getQuestionsInEvent("1cfs");
+		const parsedQuestion = JSON.parse(JSON.stringify(questions));
+		const compare = [];
+
+		parsedQuestion[0].Questions.forEach(elem => compare.push(elem.id));
+		compare.should.be.eql([27, 50, 96, 279, 298]);
+	});
+	it("get question Id from EventCode dutu", async () => {
+		const questions = getQuestionsInEvent("dutu");
+		const parsedQuestion = JSON.parse(JSON.stringify(questions));
+		const compare = [];
+
+		parsedQuestion[0].Questions.forEach(elem => compare.push(elem.id));
+		compare.should.be.eql([212, 220]);
+	});
+	it("get question Id from EventCode uomf", async () => {
+		const questions = getQuestionsInEvent("uomf");
+		const parsedQuestion = JSON.parse(JSON.stringify(questions));
+		const compare = [];
+
+		parsedQuestion[0].Questions.forEach(elem => compare.push(elem.id));
+		compare.should.be.eql([172, 228]);
 	});
 });
