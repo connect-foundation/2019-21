59924c9a6a223a49c015d95f56217ae5514e29f3
YujunKim <wnsqlehlswk@naver.com>
2019-11-15 00:54:51 +0900

    fix: 배포시 container 내부 및 외부에서 접근 가능하도록 수정

--- backend/DB/config/mySQL.prod.config.js
+++ backend/DB/config/mySQL.prod.config.js
@@ -1,24 +1,25 @@
 require("dotenv").config();
 
-const host = process.env.MYSQL_PROD_HOST;
 const user = process.env.MYSQL_PROD_USER;
 const password = process.env.MYSQL_PROD_PASSWORD;
 const scheme = process.env.MYSQL_PROD_SCHEME;
 const dialect = process.env.MYSQL_PROD_DIALECT;
 const port = process.env.MYSQL_PROD_PORT;
+const containerName = process.env.MYSQL_PROD_CONTAINER_NAME;
 const pool = {
-    max: 5,
-    min: 0,
-    acquire: 30000,
-    idle: 10000,
+	max: 5,
+	min: 0,
+	acquire: 30000,
+	idle: 10000,
 };
 
 export const config = {
-    host,
-    user,
-    password,
-    scheme,
-    dialect,
-    port,
-    pool,
+	host,
+	user,
+	password,
+	scheme,
+	dialect,
+	port,
+	pool,
+	containerName,
 };
