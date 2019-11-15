03d406fc8376fe6ff3f80f4611c378790d857689
hkwon <mr.hyuk.kwon@gmail.com>
2019-11-15 13:54:45 +0900

    Review 요청합니다.
    파일 첫줄에 comment 로 review 받고자 하는 내용을 기술했습니다. 감사합니다.

--- frontend/main-app/src/App.js
+++ frontend/main-app/src/App.js
@@ -1,3 +1,10 @@
+/**
+ * Review 요청
+ * line #: 27
+ * 모달을 띄우는 방식을 이런식으로 코딩하면 되는지 궁금합니다.
+ **/ 
+
+
 import React, {useState} from "react";
 import "./App.css";
 import EventForm from "./compounds/EventForm";
