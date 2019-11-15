d3f26de18b90917030d5ee6c38abf97fc5605be8
김근영 (노움킹김노움) <emsud@inu.ac.kr>
2019-11-15 13:49:42 +0900

    코드 리뷰용 커밋

--- backend/DB/queries/event.js
+++ backend/DB/queries/event.js
@@ -1,41 +1,43 @@
 import models from "../models";
 
+//review
+
 module.exports = class EventQuery {
-    constructor() {}
+	constructor() {}
 
-    async getIdByCode(code) {
-        const event = await models.Event.findAll({
-            where: {
-                code,
-            },
-            attributes: ["id"],
-        });
+	async getIdByCode(code) {
+		const event = await models.Event.findAll({
+			where: {
+				code,
+			},
+			attributes: ["id"],
+		});
 
-        return event;
-    }
+		return event;
+	}
 
-    async getQuestionsInEvent(code) {
-        const question = await models.Event.findAll({
-            include: [
-                {
-                    model: models.Question,
-                    include: [
-                        {
-                            model: models.Emoji,
-                        },
-                        {
-                            model: models.Question,
-                        },
-                        {
-                            model: models.Like,
-                        },
-                    ],
-                },
-            ],
-            where: {
-                code: code,
-            },
-        });
-        return question;
-    }
+	async getQuestionsInEvent(code) {
+		const question = await models.Event.findAll({
+			include: [
+				{
+					model: models.Question,
+					include: [
+						{
+							model: models.Emoji,
+						},
+						{
+							model: models.Question,
+						},
+						{
+							model: models.Like,
+						},
+					],
+				},
+			],
+			where: {
+				code: code,
+			},
+		});
+		return question;
+	}
 };
