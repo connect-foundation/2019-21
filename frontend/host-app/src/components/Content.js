0e49caf57a9d16b76bbaf51a9e6bcf26b3dde41b
Tei <teihong93@gmail.com>
2019-11-18 15:48:48 +0900

    feat: host-app event 상태에 따라 이벤트 생성 버튼 출력 구현#110
    issue: #110 기본 layout 구성
    App.js 에 event Status 를 추가하여
    event 가 있다면 content 를 보여주고
    이벤트가 없다면 event 생성 버튼을 보여주는 UI
    작성.

--- frontend/host-app/src/components/Content.js
+++ frontend/host-app/src/components/Content.js
@@ -1,6 +1,7 @@
 import React from "react";
 import styled from "styled-components";
 import Column from "./Column";
+import EmptyContent from "./EmptyContent";
 
 const ContentStyle = styled.div`
 	display: flex;
@@ -12,14 +13,18 @@ const ContentStyle = styled.div`
 	padding: 4px 8px;
 `;
 
-function Content() {
-	return (
+function Content({event}) {
+	return event ? (
 		<ContentStyle>
 			<Column title="검열중" />
 			<Column title="최신 질문" />
 			<Column title="인기 질문" />
 			<Column title="완료 질문" />
 			<Column title="투표" />
+		</ContentStyle>
+	) : (
+		<ContentStyle>
+			<EmptyContent/>
 		</ContentStyle>
 	);
 }
---
+++ frontend/host-app/src/components/EmptyContent.js
@@ -0,0 +1,42 @@
+import React from "react";
+import styled from "styled-components";
+import {Button} from "@material-ui/core";
+
+const EmptyContentBox = styled.div`
+	display: flex;
+	flex-direction: column;
+	flex: 1;
+	overflow: auto;
+	align-items: center;
+	border-radius: 8px;
+	background-color: #f1f3f5;
+	border: 1px solid #e9ecef;
+	height: 100%;
+	box-sizing: border-box;
+`;
+
+const EmptyContentDiv = styled.div`
+    display: flex;
+	flex-direction: column;
+	font-size:2rem;
+	margin: auto 0;
+`;
+
+function Column() {
+	return (
+		<EmptyContentBox>
+			<EmptyContentDiv>
+                현재 진행중인 이벤트가 없습니다
+				<Button
+					size="medium"
+					variant="contained"
+					color="primary"
+				>
+                    이벤트 만들기
+				</Button>
+			</EmptyContentDiv>
+		</EmptyContentBox>
+	);
+}
+
+export default Column;
