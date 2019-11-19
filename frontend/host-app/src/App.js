168added06fdb96215fad51d7dd073fc7f8d0738
hkwon <mr.hyuk.kwon@gmail.com>
2019-11-19 20:10:50 +0900

    chore: host-app/App.js 수정
    NewPollModal 컴포넌트를 띄우기 위해 임시로 수정함

--- frontend/host-app/src/App.js
+++ frontend/host-app/src/App.js
@@ -3,13 +3,17 @@ import "./App.css";
 import Header from "./components/Header";
 import Nav from "./components/Nav";
 import Content from "./components/Content";
+import NewPollModal from "./components/Poll/NewPollModal";
 
 function App() {
+	const modal = true;
+
 	return (
 		<div className="App">
 			<Header />
 			<Nav />
 			<Content />
+			{modal && <NewPollModal />}
 		</div>
 	);
 }
