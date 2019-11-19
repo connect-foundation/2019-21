060b62e0d4587e74e9d81c93ec431762620f807a
Yujun Kim <wnsqlehlswk@naver.com>
2019-11-19 13:57:30 +0900

    refactoring: decompose component of LeftNavMenuBar

--- frontend/guest-app/src/components/NavBar/LeftNavMenuBar.js
+++ frontend/guest-app/src/components/NavBar/LeftNavMenuBar.js
@@ -1,59 +1,61 @@
+import React from "react";
 import Drawer from "@material-ui/core/Drawer";
 import List from "@material-ui/core/List";
 import ListItem from "@material-ui/core/ListItem";
 import ListItemIcon from "@material-ui/core/ListItemIcon";
 import ListItemText from "@material-ui/core/ListItemText";
 import Divider from "@material-ui/core/Divider";
-import React from "react";
 import makeStyles from "@material-ui/core/styles/makeStyles.js";
 
-const useStyles = makeStyles({
+const useSideMenuStyles = makeStyles({
 	list: {
 		width: 250,
 	},
-	fullList: {
-		width: "auto",
-	},
 });
 
+function MenuItem({icon, itemText}) {
+	return (
+		<ListItem button key={"edit my profile"}>
+			<ListItemIcon>{icon}</ListItemIcon>
+			<ListItemText primary={itemText} />
+		</ListItem>
+	);
+}
+
+function MenuList({classes, toggleNavMenu}) {
+	return (
+		<div
+			className={classes.list}
+			role="presentation"
+			onClick={toggleNavMenu}
+			onKeyDown={toggleNavMenu}
+		>
+			<List>
+				<Divider />
+				<MenuItem
+					icon={<i className="fas fa-user" />}
+					itemText={"edit my profile"}
+				/>
+				<MenuItem
+					icon={<i className="fas fa-comment-dots" />}
+					itemText={"my questions"}
+				/>
+				<MenuItem
+					icon={<i className="fas fa-sign-out-alt" />}
+					itemText={"logout"}
+				/>
+			</List>
+			<Divider />
+		</div>
+	);
+}
+
 export function LeftSideNavMenu({state, toggleNavMenu}) {
-	const classes = useStyles();
+	const classes = useSideMenuStyles();
 
 	return (
 		<Drawer open={state} onClose={toggleNavMenu}>
-			{
-				<div
-					className={classes.list}
-					role="presentation"
-					onClick={toggleNavMenu}
-					onKeyDown={toggleNavMenu}
-				>
-					<List>
-						<Divider />
-						<ListItem button key={"edit my profile"}>
-							<ListItemIcon>
-								<i className="fas fa-user" />
-							</ListItemIcon>
-							<ListItemText primary={"edit my profile"} />
-						</ListItem>
-
-						<ListItem button key={"my questions"}>
-							<ListItemIcon>
-								<i className="fas fa-comment-dots" />
-							</ListItemIcon>
-							<ListItemText primary={"my questions"} />
-						</ListItem>
-
-						<ListItem button key={"logout"}>
-							<ListItemIcon>
-								<i className="fas fa-sign-out-alt" />
-							</ListItemIcon>
-							<ListItemText primary={"logout"} />
-						</ListItem>
-					</List>
-					<Divider />
-				</div>
-			}
+			<MenuList {...{classes, toggleNavMenu}} />
 		</Drawer>
 	);
 }
