4d1aa7a8de68fa36ffa1dd4eaa6ee8237358886e
hkwon <mr.hyuk.kwon@gmail.com>
2019-11-19 20:05:26 +0900

    feat: PollName 컴포넌트 추가
    투표 이름을 설정하는 컴포넌트

---
+++ frontend/host-app/src/components/Poll/PollName.js
@@ -0,0 +1,29 @@
+import React from "react";
+import styled from "styled-components";
+import {TextField} from "@material-ui/core";
+
+const RowWrapper = styled.div`
+    display: flex;
+    flex-direction: row;
+	align-items: center;
+	justify-content: ${props => (props.left ? "flex-start" : "space-around")};
+	width: 100%;
+	min-height: 60px;
+	padding: 0 2rem;
+	box-sizing: border-box;
+`;
+
+function PollName({value, onChange}) {
+	return (<RowWrapper left>
+		<span>투표 제목 :</span><span>&nbsp;&nbsp;&nbsp;</span>
+		<TextField
+			margin="normal"
+			variant="outlined"
+			placeholder="투표 제목을 입력해주세요"
+			value={value}
+			onChange={onChange}
+		/>
+	</RowWrapper>);
+}
+
+export default PollName;
