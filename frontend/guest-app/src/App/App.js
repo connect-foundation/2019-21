3e8a28df50d27d95f5088de62bf1299b7f4034ec
Yujun Kim <wnsqlehlswk@naver.com>
2019-11-20 14:02:24 +0900

    feature: add NavBar and tabBar at App.js

--- frontend/guest-app/src/App/App.js
+++ frontend/guest-app/src/App/App.js
@@ -1,57 +1,13 @@
 import React from "react";
 import "./App.css";
-import QuestionContainer from "../components/Question/QuestionContainer.js";
-import TabPanel from "../components/Tab/TabPanel.js";
 import NavBar from "../components/NavBar/NavBar.js";
-import TabBar from "../components/Tab/TabBar.js";
-import {
-	LeftSideNavMenu,
-	useLeftMenuBarState,
-} from "../components/NavBar/LeftNavMenuBar.js";
-import useTabBarState from "../components/Tab/TabBarState.js";
-import useModalState from "../components/useModalState.js";
-import LogOutModal from "../components/NavBar/LogoutModal.js";
+import Tab from "../components/Tab/Tab.js";
 
 export default function App() {
-	const navBarState = useLeftMenuBarState();
-	const tabBarState = useTabBarState();
-	const editProfileModalState = useModalState();
-	const myQuestionModalState = useModalState();
-	const logoutModalState = useModalState();
-
 	return (
 		<div>
-			<NavBar onToggleNavClick={navBarState.toggleNavMenu} />
-			<LeftSideNavMenu
-				isOpen={navBarState.isOpen}
-				toggleNavMenu={navBarState.toggleNavMenu}
-			/>
-			<TabBar
-				{...{
-					tabIdx: tabBarState.tabIdx,
-					onTabIdxChange: tabBarState.selectTabIdx,
-				}}
-			/>
-			<TabPanel value={tabBarState.tabIdx} index={0}>
-				<QuestionContainer />
-			</TabPanel>
-			<TabPanel value={tabBarState.tabIdx} index={1}>
-				<div>vote me</div>
-			</TabPanel>
-
-			<LeftSideNavMenu
-				isOpen={navBarState.isOpen}
-				toggleNavMenu={navBarState.toggleNavMenu}
-				onEditProfileClick={editProfileModalState.openModal}
-				onMyQuestionClick={myQuestionModalState.openModal}
-				onLogoutClick={logoutModalState.openModal}
-			/>
-
-			<LogOutModal
-				isOpened={logoutModalState.isOpened}
-				onCancelClick={logoutModalState.closeModal}
-				onConfirmClick={logoutModalState.closeModal}
-			/>
+			<NavBar />
+			<Tab />
 		</div>
 	);
 }
