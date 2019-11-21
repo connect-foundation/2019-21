2e38f8362237fa200b84fa4a4c7e342a02e239a6
Yujun Kim <wnsqlehlswk@naver.com>
2019-11-21 17:31:28 +0900

    feature: 질문 탭 헤더

--- frontend/guest-app/src/components/Question/QuestionContainer.js
+++ frontend/guest-app/src/components/Question/QuestionContainer.js
@@ -2,14 +2,22 @@ import React, {useState} from "react";
 import styled from "styled-components";
 import QuestionCard from "./QuestionCard.js";
 import DummyData from "./QuestionDummyData.js";
+import QuestionContainerHeader from "./QuestionContainerHeader.js";
+import useTabGroup from "../TabGroup/useTabGroup.js";
 
 const Container = styled.div``;
 
 function QuestionContainer(props) {
 	const [datas] = useState({questions: DummyData()});
+	const {tabIdx, selectTabIdx} = useTabGroup();
 
 	return (
 		<Container>
+			<QuestionContainerHeader
+				questionNumber={datas.questions.length}
+				tabIdx={tabIdx}
+				onSelectTab={selectTabIdx}
+			/>
 			{datas.questions.map((question, idx) => (
 				<QuestionCard {...question} key={idx} />
 			))}
