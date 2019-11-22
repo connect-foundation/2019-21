9920ae504974ce6921afcf0f5f51774d37b27caa
김근영 (노움킹김노움) <emsud@inu.ac.kr>
2019-11-22 10:56:02 +0900

    week3 코드리뷰 요청합니다

---
+++ TabNavigation.js
@@ -0,0 +1,52 @@
+import React from "react";
+import { Tab, Box, Tabs } from "@material-ui/core";
+import { styled } from "@material-ui/core/styles";
+import TabContent from "./TabContent";
+import GeneralSetting from "./GeneralSetting/GeneralSetting";
+import AdvanceSetting from "./AdvanceSetting/AdvanceSetting";
+
+const MyContainer = styled(Box)({
+	display: "flex",
+	flexShrink: 1,
+	padding: 0,
+});
+
+const CustomTab = styled(Tab)({});
+
+function allyProps(index) {
+	return {
+		id: `setting-tab-${index}`,
+		"aria-controls": `setting-tabpanel-${index}`,
+	};
+}
+
+export default function TabNavigation({ handleClose }) {
+	const [value, setValue] = React.useState(1);
+
+	const handleChange = (event, newValue) => {
+		setValue(newValue);
+	};
+
+	return (
+		<MyContainer>
+			<Tabs
+				orientation="vertical"
+				value={value}
+				onChange={handleChange}
+				aria-label="simple tabs example"
+			>
+				/
+				<CustomTab label="이벤트설정" disabled={true} />
+				<CustomTab label="기본설정" {...allyProps(0)} />
+				<CustomTab label="상세설정" {...allyProps(1)} />
+			</Tabs>
+
+			<TabContent value={value} index={1}>
+				<GeneralSetting handleClose={handleClose} />
+			</TabContent>
+			<TabContent value={value} index={2}>
+				<AdvanceSetting handleClose={handleClose} />
+			</TabContent>
+		</MyContainer>
+	);
+}
