a225532e14e598236296ee23a146e82c14e82fdf
Yujun Kim <wnsqlehlswk@naver.com>
2019-11-25 01:41:39 +0900

    refactoring: serviceWorker.js를 libs/... 으로 이동

--- frontend/main-app/src/index.js
+++ frontend/main-app/src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-import * as serviceWorker from "./serviceWorker";
+import * as serviceWorker from "./libs/serviceWorker.js";
 
 ReactDOM.render(<App />, document.getElementById("root"));
 
