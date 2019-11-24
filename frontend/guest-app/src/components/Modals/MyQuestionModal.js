1c0548a3f5d9195c414f11f91a1a4395012bb2fc
Yujun Kim <wnsqlehlswk@naver.com>
2019-11-25 01:55:45 +0900

    refactoring: MyQuestionModal 내부를 QuestionCardList 로 교체

--- frontend/guest-app/src/components/Modals/MyQuestionModal.js
+++ frontend/guest-app/src/components/Modals/MyQuestionModal.js
@@ -2,8 +2,8 @@ import React, {useState} from "react";
 import {Scrollbars} from "react-custom-scrollbars";
 import {Typography} from "@material-ui/core";
 import CommonModal from "../CommonModal/CommonModal.js";
-import QuestionCard from "../Question/QuestionCard/QuestionCard.js";
 import DummyData from "../Question/QuestionDummyData.js";
+import QuestionCardList from "../Question/QuestionCardList.js";
 
 function MyQuestionModal({isOpened, onCancelClick}) {
 	const [datas] = useState({questions: DummyData()});
@@ -12,9 +12,7 @@ function MyQuestionModal({isOpened, onCancelClick}) {
 		<CommonModal isOpened={isOpened} onCancelClick={onCancelClick}>
 			<Typography>My question</Typography>
 			<Scrollbars style={{width: "100%", height: "400px"}}>
-				{datas.questions.map((question, idx) => (
-					<QuestionCard {...question} key={idx} />
-				))}
+				<QuestionCardList questions={datas.questions} />
 			</Scrollbars>
 		</CommonModal>
 	);
