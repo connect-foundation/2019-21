0bc4e0e23442a98af011a7db453b946563967ac8
Tei <teihong93@gmail.com>
2019-11-27 15:37:47 +0900

    add: backend/express/utils.js
    backend/express 에 utils.js 추가
    sequelize 에서 받은 data 를 가공하는 함수를
    작성하는 용도

--- backend/express/app.js
+++ backend/express/app.js
@@ -1,8 +1,8 @@
 import { config } from "dotenv";
 import express from "express";
 import EventQuery from "../DB/queries/event";
-import io from "socket.io";
 import http from "http";
+import getSequelizeData from "./utils";
 
 config();
 
@@ -24,7 +24,7 @@ app.get("/test/:code", async (req, res, next) => {
 	try {
 		const eventQuery = new EventQuery();
 		const questions = await eventQuery.getQuestionsInEvent(req.params.code);
-
+		console.log(getSequelizeData(questions)[0].Questions);
 		return res.json(questions);
 	} catch (e) {
 		return next(e);
@@ -34,29 +34,6 @@ app.get("/test/:code", async (req, res, next) => {
 const httpServer = http.createServer(app).listen(3000, () => {
 	console.log(`start server at 3000 with ${process.env.NODE_ENV} mode`);
 });
-const socketServer = io(httpServer);
-socketServer.on("connection", socket => {
-	console.log("소켓 IO 연결");
-	socket.on("EMIT", req => {
-		console.log("sdfsdfsdf");
-		console.log(req);
-		socketServer.emit("EMIT", req);
-	});
-});
 
-const nameSpaceServer = socketServer.of("/nameSpace");
-nameSpaceServer.on("connect", function(socket) {
-	console.log("네임스페이스 접속");
-	socket.on("ROOM", req => {
-		// client 에서 받은 data에 있는 room id를 받아옴
-		const room = (socket.room = req.data);
-		// 해당 id Room 에 들어감
-		socket.join(room);
-		// room 에 있는 모든 client 에게 메세지 전송
-		nameSpaceServer.to(room).emit("room_msg", "this is room msg");
-		//이제 최초 room 버튼을 눌러 room 에 입장한 이후에는 다른 client가 room 버튼을
-		//누를때 마다 그 방에 있는 모든 client의 콘솔이 출력된다.
-	});
-});
 
 export default app;
