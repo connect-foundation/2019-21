ea5d3b919223fa36f908f3db27f7d7b29491d8a5
Yujun Kim <wnsqlehlswk@naver.com>
2019-11-27 14:25:02 +0900

    refactoring: host app의 App 관련 파일 디렉토리 변경
    ./src/... -> ./src/App/...

--- frontend/host-app/src/index.js
+++ frontend/host-app/src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import App from "./App";
+import App from "./App/App.js";
 import * as serviceWorker from "./libs/serviceWorker.js";
 
 ReactDOM.render(<App />, document.getElementById("root"));
