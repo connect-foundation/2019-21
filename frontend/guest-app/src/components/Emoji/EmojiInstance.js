627a0dfda1920f8de9ede98e686000ca6119f481
hkwon <mr.hyuk.kwon@gmail.com>
2019-11-28 09:44:36 +0900

    enhance: 내가 클릭한 Emoji 색상 변경
    ${props => props.voted && css`
          background-color: #e7f5ff; /* blue0 */
          border: 1px solid #339af0; /* blue5 */
    `}

--- frontend/guest-app/src/components/Emoji/EmojiInstance.js
+++ frontend/guest-app/src/components/Emoji/EmojiInstance.js
@@ -1,11 +1,14 @@
 import React from "react";
-import styled from "styled-components";
+import styled, {css} from "styled-components";
 import {Emoji} from "emoji-mart";
 
 const EmojiInstanceStyle = styled.div`
     display: inline-block;
     border: 1px solid #dee2e6; /* gray3 */
-    background-color: ${props => (props.voted ? "#dee2e6" : "inherit")}; /* gray3 */
+    ${props => props.voted && css`
+        background-color: #e7f5ff; /* blue0 */
+        border: 1px solid #339af0; /* blue5 */
+    `}
     padding: 0 0.2rem;
     border-radius: 0.5rem;
 	&:hover {
