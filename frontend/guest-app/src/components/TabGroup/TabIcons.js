e2289372bb29dffc06cfb0307c04f6d05ec2575a
YujunKim <wnsqlehlswk@naver.com>
2019-11-30 01:38:27 +0900

    refactoring: tabIcon 조건부 랜더링 개선

--- frontend/guest-app/src/components/TabGroup/TabIcons.js
+++ frontend/guest-app/src/components/TabGroup/TabIcons.js
@@ -3,38 +3,26 @@ import Badge from "@material-ui/core/Badge/Badge.js";
 import QuestionAnswerIcon from "@material-ui/icons/QuestionAnswer";
 import PollIcon from "@material-ui/icons/Poll";
 
+const style = {marginRight: "8px"};
+
 export function QnATabIcon({showBadge}) {
-	if (showBadge) {
-		return (
-			<Badge color="error" variant="dot">
-				<QuestionAnswerIcon />
-				Q&A
-			</Badge>
-		);
-	} else {
-		return (
-			<span>
-				<QuestionAnswerIcon />
-				Q&A
-			</span>
-		);
-	}
+	const props = showBadge ? {color: "error", variant: "dot"} : {};
+
+	return (
+		<Badge {...props}>
+			<QuestionAnswerIcon style={style} />
+			Q&A
+		</Badge>
+	);
 }
 
 export function PollTabIcon({showBadge}) {
-	if (showBadge) {
-		return (
-			<Badge color="error" variant="dot">
-				<PollIcon />
-				Poll
-			</Badge>
-		);
-	} else {
-		return (
-			<span>
-				<PollIcon />
-				Poll
-			</span>
-		);
-	}
+	const props = showBadge ? {color: "error", variant: "dot"} : {};
+
+	return (
+		<Badge {...props}>
+			<PollIcon style={style} />
+			Poll
+		</Badge>
+	);
 }
