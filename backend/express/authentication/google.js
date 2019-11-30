e733eacd19f49ea317750c8354881553136c1256
김근영 (노움킹김노움) <emsud@inu.ac.kr>
2019-11-30 22:06:35 +0900

    feat: add google oauth for jwt
    jwt를 사용하기 위해 passport init 부분 수정.
    session을 사용하지 않고 사용자가 구글로 로그인했을 떄 email과 id,name을 통해 DB에 접근하고 사용자가 있으면  사용자 정보를 가져옴
    사용자가 없을 시 DB에 로그인 정보를 저장.

---
+++ backend/express/authentication/google.js
@@ -0,0 +1,40 @@
+import passport from "passport";
+import { Strategy } from "passport-google-oauth20";
+import loadConfig from "../config/configLoader";
+import { createHost, findHostById } from "../../DB/queries/host";
+
+const GoogleStrategy = Strategy;
+
+function extractProfile(profile) {
+	let imageUrl = "";
+	if (profile.photos && profile.photos.length) {
+		imageUrl = profile.photos[0].value;
+	}
+	return {
+		id: profile.id,
+		displayName: profile.displayName,
+		image: imageUrl,
+		email: profile.emails[0].value,
+	};
+}
+
+export default (function() {
+	const { oAuthArgs } = loadConfig();
+	passport.use(
+		new GoogleStrategy(
+			{ ...oAuthArgs },
+			async (accessToken, refreshToken, profile, cb) => {
+				try {
+					const { id, displayName, image, email } = extractProfile(
+						profile
+					);
+					let user = await findHostById(id);
+					if (!user) user = await createHost(id, displayName, email);
+					return cb(null, user);
+				} catch (error) {
+					console.error(error);
+				}
+			}
+		)
+	);
+})();
