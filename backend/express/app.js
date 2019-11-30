a6acaa1f921294ba81f467bbfd1525a81b5a83b6
김근영 (노움킹김노움) <emsud@inu.ac.kr>
2019-11-30 22:50:54 +0900

    feat: 인증을 위한 oAuth JWT 인증 방식으로 변경
    기존 memcache를 이용한 세션 인증 방식에서 JWT를 활용한 방식으로 변경 JWT는 access token만 client 쿠키에 저장.

--- backend/express/app.js
+++ backend/express/app.js
@@ -1,42 +1,31 @@
 import { config } from "dotenv";
 import express from "express";
-import session from "express-session";
 import passport from "passport";
 import loadConfig from "./config/configLoader.js";
 import applyStaticAppServing from "./middleware/applyStaticAppServing.js";
 import morgan from "morgan";
-import * as auth from "./auth/passport";
-import connect_memjs from "connect-memjs";
+import * as google from "./authentication/google";
+import * as jwt from "./authentication/jwt";
 import authRouter from "./routes/auth";
 
 config();
 
-const { port, publicPath, mechacheArgs } = loadConfig();
-const MemcachedStore = connect_memjs(session);
-const sessionConfig = {
-	resave: false,
-	saveUninitialized: false,
-	secret: "SECRTET",
-	signed: true,
-	store: new MemcachedStore({
-		mechacheArgs,
-	}),
-};
-
+const { port, publicPath } = loadConfig();
 const app = express();
-
-app.use(session(sessionConfig));
-app.use(passport.initialize());
-app.use(passport.session());
-app.use(morgan("dev"));
-app.use("/auth", authRouter);
-
 applyStaticAppServing(app, publicPath);
 
-app.get("/", async (req, res) => {
-	console.log(req.session);
-	res.send("ok");
-});
+app.use(passport.initialize());
+app.use(morgan("dev"));
+
+app.use("/auth", authRouter);
+
+app.get(
+	"/",
+	passport.authenticate("jwt", { session: false }),
+	async (req, res) => {
+		res.send("ok");
+	}
+);
 
 app.listen(port, () => {
 	console.log(
