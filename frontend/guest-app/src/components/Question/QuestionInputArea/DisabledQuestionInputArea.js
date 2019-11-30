32f11b7f35387457c6a61bba5acb078ff909cd29
YujunKim <wnsqlehlswk@naver.com>
2019-12-01 07:59:10 +0900

    enhance: 질문 추가 컴포넌트 UI 개선
    컴포넌트를 카드로 변경하여 가시성 향상
    내부 구분선 추가

--- frontend/guest-app/src/components/Question/QuestionInputArea/DisabledQuestionInputArea.js
+++ frontend/guest-app/src/components/Question/QuestionInputArea/DisabledQuestionInputArea.js
@@ -1,20 +1,15 @@
 import React from "react";
 import PropTypes from "prop-types";
-import {CardContent} from "@material-ui/core";
-import Divider from "@material-ui/core/Divider";
 import EditIcon from "@material-ui/icons/Edit";
 
 function DisabledQuestionInputArea(props) {
 	const {onClick} = props;
 
 	return (
-		<>
-			<Divider/>
-			<CardContent onClick={onClick}>
-				<EditIcon style={{marginRight: "8px"}}/>
-        질문하기
-			</CardContent>
-		</>
+		<div onClick={onClick}>
+			<EditIcon style={{marginRight: "8px"}} />
+			질문하기
+		</div>
 	);
 }
 
