e8b289c3c2896f11a423a25624b2a21d4af244e4
YujunKim <wnsqlehlswk@naver.com>
2019-11-30 17:40:08 +0900

    enhance: QuestionContainerHeader 에 proptypes, defaultProp 추가

--- frontend/guest-app/src/components/Question/QuestionContainerHeader.js
+++ frontend/guest-app/src/components/Question/QuestionContainerHeader.js
@@ -3,8 +3,11 @@ import Paper from "@material-ui/core/Paper";
 import {Typography} from "@material-ui/core";
 import Tab from "@material-ui/core/Tab";
 import Tabs from "@material-ui/core/Tabs";
+import PropTypes from "prop-types";
 
-function QuestionContainerHeader({questionNumber, tabIdx, onSelectTab}) {
+function QuestionContainerHeader(props) {
+	const {questionNumber, tabIdx, onSelectTab} = props;
+
 	return (
 		<Paper>
 			<Tabs value={tabIdx} onChange={onSelectTab}>
@@ -26,5 +29,16 @@ function QuestionContainerHeader({questionNumber, tabIdx, onSelectTab}) {
 		</Paper>
 	);
 }
+
+QuestionContainerHeader.propTypes = {
+	questionNumber: PropTypes.number,
+	tabIdx: PropTypes.number,
+	onSelectTab: PropTypes.func,
+};
+
+QuestionContainerHeader.defaultProps = {
+	questionNumber: 0,
+	tabIdx: 0,
+};
 
 export default QuestionContainerHeader;
