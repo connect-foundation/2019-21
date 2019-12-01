8aba8de02e8d0c02dfa02f59114151bc26467c9d
김근영 (노움킹김노움) <emsud@inu.ac.kr>
2019-12-01 23:13:52 +0900

    feat: 설정에 따라 apollo client를 생성 하도록 구현
    auth 설정과 아닐 때 apollo client 생성 방식이 다르기 때문에 인자값에 따라 이를 분기하도록 처리
    uri과 token이 파라미터로 들어오면 apollo client에 auth header를 추가하여 연결하도록 구현

---
+++ frontend/host-app/src/libs/createApolloClient.js
@@ -0,0 +1,28 @@
+import { ApolloClient } from "apollo-client";
+import { createHttpLink } from "apollo-link-http";
+import { InMemoryCache } from "apollo-cache-inmemory";
+import { setContext } from "apollo-link-context";
+
+export default function creaetApolloClient(uri, token) {
+	const httpLink = createHttpLink({ uri });
+	if (token) {
+		const authLink = setContext((_, { headers }) => {
+			return {
+				headers: {
+					...headers,
+					authorization: `Bearer ${token}`,
+				},
+			};
+		});
+
+		return new ApolloClient({
+			link: authLink.concat(httpLink),
+			cache: new InMemoryCache(),
+		});
+	} else {
+		return new ApolloClient({
+			link: httpLink,
+			cache: new InMemoryCache(),
+		});
+	}
+}
