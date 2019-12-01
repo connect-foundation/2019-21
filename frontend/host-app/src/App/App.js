66a94c75a7ba431a89daf0dad15b3e7ef31a8100
Tei <teihong93@gmail.com>
2019-12-01 17:17:25 +0900

    env: fost-app apollo config 파일 작성
    host-app 에서 graphQl 을 사용하기 위한 config
    설정 및 환경파일 작성

--- frontend/host-app/src/App/App.js
+++ frontend/host-app/src/App/App.js
@@ -1,21 +1,32 @@
 import React, {useState} from "react";
+import ApolloClient from "apollo-boost";
+import {ApolloProvider} from "@apollo/react-hooks";
 import "./App.css";
 import Header from "../components/Header";
 import Nav from "../components/Nav";
 import Content from "../components/Content";
 import NewPollModal from "../components/Poll/NewPollModal";
+import configLoader from "../config/configLoader.js";
+
+
+const config = configLoader();
+const apolloClient = new ApolloClient({
+	uri: config.apolloURI,
+});
 
 function App() {
 	const modal = false;
 	const [event] = useState(true);
 
 	return (
-		<div className="App">
-			<Header />
-			<Nav />
-			{modal && <NewPollModal />}
-			<Content event={event} />
-		</div>
+		<ApolloProvider client={apolloClient}>
+			<div className="App">
+				<Header />
+				<Nav />
+				{modal && <NewPollModal />}
+				<Content event={event} />
+			</div>
+		</ApolloProvider>
 	);
 }
 
