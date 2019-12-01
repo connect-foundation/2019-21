4f68ed30fd0e0b6f47ec1d40e24a30ed271b60b8
김근영 (노움킹김노움) <emsud@inu.ac.kr>
2019-12-01 23:22:31 +0900

    refactor: 필요없는 함수 제거

--- backend/graphQL/model/event/event.resolver.js
+++ backend/graphQL/model/event/event.resolver.js
@@ -1,16 +1,9 @@
 import query from "../../../DB/queries/event";
 
-async function DBquery(hostId) {
-	const dbQueryValue = await query.getEventsByHost(hostId);
-	return dbQueryValue;
-}
-
-async function questionResolver(hostId) {
-	return DBquery(hostId);
-}
-
 export default {
 	Query: {
-		events: (_, { hostId }) => questionResolver(hostId),
+		events: async (_, { hostId }) => {
+			return await query.getEventsByHost(hostId);
+		},
 	},
 };
