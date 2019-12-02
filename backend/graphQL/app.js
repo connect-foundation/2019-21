8d40649f34038466b192cc65ee973c0261c0a440
김근영 (노움킹김노움) <emsud@inu.ac.kr>
2019-12-03 00:40:15 +0900

    fix: auth 관련 middlewear 수정
    graph ql auth 관련해서 기존 passport-jwt 사용을 jwt-verify로 변경.
    graphql 미들웨어를 추가하여 토큰값에 따라 인증허가 로직 구현.
    waring: 미들웨어에서 인증허가는 들어온 토큰 값에 따라 사용자를 구분하고 실제로 허가 부분은 리졸버단에서 구현해야함.

--- backend/graphQL/app.js
+++ backend/graphQL/app.js
@@ -4,36 +4,60 @@ import cookieParser from "cookie-parser";
 import resolvers from "./resolvers.js";
 import typeDefs from "./typeDefs.js";
 import config from "./config.js";
-import passport from "passport";
-import * as jwt from "./authentication/jwt";
+import jwt from "jsonwebtoken";
 import cors from "cors";
+import { findHostById } from "../DB/queries/host";
+
+/*
+	context = payload:
+				{ 
+					iat: 1575295973,
+					exp: 1575299573,
+					aud: 'host',
+					iss: 'Vaagle',
+					sub: '101530667441561687884' 
+				}
+*/
 
 const saveContext = async (resolve, root, args, context, info) => {
-	context.user = context.request.user;
-	context.auth = context.request.authInfo;
-	const result = await resolve(root, args, context, info);
+	let audience = "anonymous";
+	audience = context.payload && context.payload.aud;
+	let authority = { sub: null, info: null };
+	switch (audience) {
+		case "host":
+			const hostInfo = await findHostById(context.payload.sub);
+			authority = { sub: "host", info: hostInfo };
+			break;
+		case "guest":
+			const guestInfo = context.payload.sub;
+			authority = { sub: "guest", info: guestInfo };
+			break;
+	}
+	const result = await resolve(root, args, authority, info);
 	return result;
 };
 
 const server = new GraphQLServer({
 	typeDefs,
 	resolvers,
-	// middlewares: [saveContext],
-	// context: req => {
-	// return { ...req };
-	// },
+	middlewares: [saveContext],
+	context: ({ request }) => {
+		const token = request.headers.authorization || "";
+		console.log(token);
+		const payload =
+			token === ""
+				? undefined
+				: jwt.verify(
+						token.split(" ")[1],
+						process.env.AUTH_TOKEN_SECRET
+				  );
+		return { payload };
+	},
 });
 
 server.express.use(cors());
 server.express.use(morgan("dev"));
 server.express.use(cookieParser());
-// server.express.use(passport.initialize());
-// server.express.use(
-// 	passport.authenticate("jwt", { session: false }),
-// 	(req, res, next) => {
-// 		next();
-// 	}
-// );
 
 server.start(config, ({ port }) => {
 	console.log(`graphQL yoga Server is running on localhost:${port}`);
