4c376b72292350da6fdffc700a7c14697906a31b
YujunKim <wnsqlehlswk@naver.com>
2019-12-02 22:36:15 +0900

    env: 배포시 DB migration 추가

--- script/deploy.sh
+++ script/deploy.sh
@@ -1,4 +1,13 @@
+echo "build all"
 yarn build
 
 cd ./backend
+
+echo "clean docker volume"
+yarn docker:volumes:remove
+
+echo "start deply"
 yarn deploy
+
+echo "DB migration"
+yarn migration:production
