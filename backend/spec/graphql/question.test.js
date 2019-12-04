46f54f2fc1325b9dcb47d66026cc0f79f27194d5
Yujun Kim <wnsqlehlswk@naver.com>
2019-12-04 16:28:06 +0900

    fix: graphql question query 수정 및 테스트 코드 추가

--- backend/spec/graphql/question.test.js
+++ backend/spec/graphql/question.test.js
@@ -1,22 +1,20 @@
 import assert from "assert";
-import {describe, it} from "mocha";
-import {GQLClient} from "./graphqlTestClient.js";
+import { describe, it } from "mocha";
+import { GQLClient } from "./graphqlTestClient.js";
+import testCase from "./question.testcase.js";
 
 describe("graphql yoga question model", () => {
 	it("able query questions", async () => {
 		const queryQuestions = `
 		query getQuestions(
-			$eventCode: ID!
-			$GuestId: ID! 
+			$EventId: ID!
 		){
-			questions(eventCode: $eventCode, GuestId: $GuestId) {
+			questions(EventId: $EventId) {
 				id
 				EventId
-				guestName
 				GuestId
 				createdAt
 				content
-				isAnonymous
 				state
 				isStared
 				likeCount  
@@ -25,28 +23,10 @@ describe("graphql yoga question model", () => {
 		`;
 
 		const variables = {
-			GuestId: 22,
-			eventCode: "u959",
+			EventId: 2,
 		};
 		const res = await GQLClient.request(queryQuestions, variables);
 
-		const expect = {
-			questions: [
-				{
-					id: "19",
-					EventId: "2",
-					guestName: "Alysha",
-					GuestId: "164",
-					createdAt: "1558509195000",
-					content: "Hic accusantium deleniti error quas dolor.",
-					isAnonymous: false,
-					state: "active",
-					isStared: false,
-					likeCount: 0,
-				},
-			],
-		};
-
-		assert.deepEqual(res, expect);
+		assert.deepEqual(res, testCase.question);
 	});
 });
