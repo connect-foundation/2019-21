4c765ec066e2f77d8d8daa52032865d2b11e26ed
Yujun Kim <wnsqlehlswk@naver.com>
2019-12-04 15:47:24 +0900

    enhance: add graphql yoga server hello test

---
+++ backend/spec/graphql/hello.test.js
@@ -0,0 +1,18 @@
+import assert from "assert";
+import { GQLClient } from "./graphqlTestClient.js";
+
+describe("graphql api", () => {
+	it("able query hello", async () => {
+		const query = `
+		query {
+			hello {
+				name
+				value
+			}
+		}`;
+		const variables = undefined;
+		const res = await GQLClient.request(query, variables);
+		const expect = { hello: { name: "hello", value: 12313 } };
+		assert.deepEqual(expect, res);
+	});
+});
