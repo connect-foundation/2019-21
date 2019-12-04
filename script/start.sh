bc64987b788dda23c996d884b6b93092ef6c7471
YujunKim <wnsqlehlswk@naver.com>
2019-12-04 23:41:36 +0900

    hotfix: 모든 서버 start script 에서 yoga 서버 실행 안되는 버그 수정

--- script/start.sh
+++ script/start.sh
@@ -6,4 +6,4 @@ express_server="cd ./backend & yarn start:express"
 DB_server="cd ./backend & yarn start:DB"
 yoga_server="cd ./backend & yarn start:yoga"
 
-concurrently "$guest_app"  "$main_app"  "$host_app" "$socketIO_server" "$express_server" "$DB_server" "yoga_server"
+concurrently "$guest_app"  "$main_app"  "$host_app" "$socketIO_server" "$express_server" "$DB_server" "$yoga_server"
