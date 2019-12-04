965c8fdcf47f17ee1c3ab721b0894f2e78aa5340
Yujun Kim <wnsqlehlswk@naver.com>
2019-12-04 16:47:14 +0900

    feature: add guest graphql query, with testcode

---
+++ backend/spec/graphql/guest.test.js
@@ -0,0 +1,30 @@
+import assert from "assert";
+import {describe, it} from "mocha";
+import {GQLClient} from "./graphqlTestClient.js";
+import testCase from "./question.testcase.js";
+
+describe("graphql yoga guest model", () => {
+	it("able query questions", async () => {
+		const query = `
+		query getGuests(
+			$EventId: ID!
+		){
+			guests(EventId: $EventId) {
+				id
+				name
+				isAnonymous
+				company
+				email
+			}
+		}
+		`;
+
+		const variables = {
+			EventId: 2,
+		};
+		const res = await GQLClient.request(query, variables);
+		// console.log(res)
+
+		// assert.deepEqual(res, testCase.question);
+	});
+});
