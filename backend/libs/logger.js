6f26e2dc96e3ef152644af28e0b9c77e10efdc71
YujunKim <wnsqlehlswk@naver.com>
2019-12-07 03:00:00 +0900

    enhance: backend server 에 사용할 logger 추가
    # why
    log를 더 잘 보기 위함
    
    # what i did
    backend/libs/logger.js 에 logger 모듈 추가
    
    usage
    ex)
    const logger = getLogger("header name")
    
    logger.info("info")
    logger.error("error")
    
    package 추가
       date-utils
       winston
       winston-daily-rotate-file

---
+++ backend/libs/logger.js
@@ -0,0 +1,53 @@
+import chalk from "chalk";
+import winston from "winston";
+
+require("winston-daily-rotate-file");
+require("date-utils");
+
+function getRand() {
+	return parseInt(Math.random() * 255, 10);
+}
+
+const randomRGB = [getRand(), getRand(), getRand()];
+
+// reference from https://mungmungdog.tistory.com/34
+// add colorize https://github.com/winstonjs/winston/issues/1135
+
+/**
+ * simple logger for backend
+ *
+ *
+ * @return {function}
+ */
+export default (header = "") => {
+	const format = winston.format.combine(
+		winston.format.colorize(),
+		winston.format.timestamp(),
+		// winston.format.align(),
+		winston.format.printf(info => {
+			const {timestamp, level, message, ...args} = info;
+
+			const ts = timestamp.slice(0, 19).replace("T", " ");
+
+			const head = chalk.rgb(...randomRGB)(header);
+
+			return `${ts} | ${head} | [${level}]: ${message} ${
+				Object.keys(args).length ? JSON.stringify(args, null, 2) : ""
+			}`;
+		}),
+	);
+
+	return winston.createLogger({
+		level: "info", // 최소 레벨
+		transports: [
+			new winston.transports.DailyRotateFile({
+				filename: "log/system.log", // log 폴더에 system.log 이름으로 저장
+				zippedArchive: true, // 압축여부
+				format,
+			}),
+			new winston.transports.Console({
+				format,
+			}),
+		],
+	});
+};
