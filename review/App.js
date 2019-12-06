b4b801522c032f369887ea14fad7f79ce0d7e22c
김근영 (노움킹김노움) <emsud@inu.ac.kr>
2019-12-06 11:01:00 +0900

    review 요청

---
+++ review/App.js
@@ -0,0 +1,42 @@
+import React, {useState} from "react";
+import {useQuery} from "@apollo/react-hooks";
+import "./App.css";
+import Header from "../components/Header";
+import Nav from "../components/Nav";
+import Content from "../components/Content";
+import NewPollModal from "../components/Poll/NewPollModal";
+import {HostProvider} from "../libs/hostContext";
+import {getEventsByHost} from "../libs/gql";
+import EmptyContent from "../components/EmptyContent";
+import {socketClient} from "../libs/socket.io-Client-wrapper";
+
+function App() {
+	const modal = false;
+	const {data, loading, error} = useQuery(getEventsByHost());
+	const [events, setEvents] = useState("");
+
+	if (loading) {
+		return <p>loading...</p>;
+	} else if (error) {
+		return <p>error-page...</p>;
+	} else {
+		if (events === "") setEvents(data.init.events);
+		const hostInfo = data.init.host;
+
+		socketClient.emit("event/initOption", 2); // dummy Event Id:2
+		const eventNum = 1;
+
+		return (
+			<HostProvider value={{hostInfo, events, setEvents}}>
+				<div className="App">
+					<Header />
+					<Nav />
+					{modal && <NewPollModal />}
+					{eventNum ? <Content event={event} /> : <EmptyContent />}
+				</div>
+			</HostProvider>
+		);
+	}
+}
+
+export default App;
