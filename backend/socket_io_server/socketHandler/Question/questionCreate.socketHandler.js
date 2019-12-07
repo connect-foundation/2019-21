df6f5e107448195e5958e13c0d9d95dfee9f6d18
YujunKim <wnsqlehlswk@naver.com>
2019-12-07 12:55:33 +0900

    refactoring: questionCreate socketHandler
    why
    minor clean code and refactoring for code quality
    
    what i did
    string을 상수 변수로 분리
    guest로 부터 받은 데이터에 default값 추가를 위한 함수 추가
    
    add todo
    * 성능 개선: 위해 여러개의 DB query를 promise.all 처리해야함
    * 성능 개선: moderation기능이 on인경우 host에만 send 하도록 수정

--- backend/socket_io_server/socketHandler/Question/questionCreate.socketHandler.js
+++ backend/socket_io_server/socketHandler/Question/questionCreate.socketHandler.js
@@ -3,24 +3,56 @@ import {updateGuestById} from "../../../DB/queries/guest";
 import eventCache from "../../EventCache.js";
 import logger from "../../logger.js";
 
-const questionCreateSocketHandler = async (data, emit) => {
+const QUESTION_STATE_MODERATION = "moderation";
+
+function getNewQuestion({
+	EventId,
+	GuestId,
+	guestName,
+	content,
+	isAnonymous = false,
+	createdAt = new Date().getTime(),
+	isShowEditButton = true,
+	didILike = false,
+	likeCount = 0,
+	status = "active",
+	isStared = false,
+}) {
+	return {
+		guestName,
+		EventId,
+		GuestId,
+		createdAt,
+		content,
+		isShowEditButton,
+		isAnonymous,
+		didILike,
+		likeCount,
+		status,
+		isStared,
+	};
+}
+
+const questionCreateSocketHandler = async (data, emit, socket, server) => {
 	try {
-		const {EventId, content, GuestId, guestName} = data;
+		const newQuestion = getNewQuestion(data);
+		const {EventId, content, GuestId, guestName, isAnonymous} = newQuestion;
 
 		logger.debug(data);
 
 		const event = await eventCache.get(EventId);
 		const currentModerationOption = event.moderationOption;
-		const reqData = data;
+		const reqData = newQuestion;
 		let newData;
 
+		// todo 성능 개선: 위해 여러개의 DB query를 promise.all 처리해야함
 		if (currentModerationOption) {
-			reqData.status = "moderation";
+			reqData.status = QUESTION_STATE_MODERATION;
 			newData = await createQuestion(
 				EventId,
 				content,
 				GuestId,
-				"moderation",
+				QUESTION_STATE_MODERATION,
 			);
 		} else {
 			newData = await createQuestion(EventId, content, GuestId);
@@ -29,9 +61,12 @@ const questionCreateSocketHandler = async (data, emit) => {
 		await updateGuestById({
 			id: GuestId,
 			name: guestName,
-			isAnonymous: false,
+			isAnonymous,
 		});
+
 		reqData.id = newData.get({plain: true}).id;
+
+		// todo 성능 개선: moderation기능이 on인경우 host에만 send 하도록 수정
 		emit(reqData);
 	} catch (e) {
 		logger.error(e);
