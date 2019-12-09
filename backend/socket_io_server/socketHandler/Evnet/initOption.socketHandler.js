206aa8307450111daa31440b3186fa65c701e8f1
Tei <teihong93@gmail.com>
2019-12-09 13:34:32 +0900

    fix: moderation 기능 fix
    moderation option 을 전부 redis 에서 참조하도록 변경

--- backend/socket_io_server/socketHandler/Evnet/initOption.socketHandler.js
+++ backend/socket_io_server/socketHandler/Evnet/initOption.socketHandler.js
@@ -1,11 +1,12 @@
 import {getEventOptionByEventId} from "../../../DB/queries/event";
-import globalOption from "../../globalOption";
+
+import eventCache from "../../EventCache";
 
 const initOptionSocketHandler = async (data, emit) => {
 	try {
 		const currentState = await getEventOptionByEventId(data); // dummy event Id
 
-		globalOption.setOption(data, currentState.get({plain: true}));
+		await eventCache.set(data.eventId, currentState.get({plain: true}));
 	} catch (e) {
 		console.log(e);
 		emit({status: "error", e});
