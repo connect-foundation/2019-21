77c802ff412eb953ce411f5fcbd1dd91aadf3208
YujunKim <wnsqlehlswk@naver.com>
2019-12-10 00:55:13 +0900

    feature: Guestapp의 사이드 메뉴바의 헤더 정보를 연동

--- frontend/guest-app/src/components/SideMenu/SideMenu.js
+++ frontend/guest-app/src/components/SideMenu/SideMenu.js
@@ -1,12 +1,20 @@
-import React from "react";
+import React, {useContext} from "react";
 import Drawer from "@material-ui/core/Drawer";
 import SideMenuHeader from "./SideMenuHeader.js";
 import SideMenuBody from "./SideMenuBody.js";
+import {GuestGlobalContext} from "../../libs/guestGlobalContext.js";
 
 export function SideMenu({isOpen = false, toggleNavMenu}) {
+	const {event} = useContext(GuestGlobalContext);
+
 	return (
 		<Drawer open={isOpen} onClose={toggleNavMenu}>
-			<SideMenuHeader />
+			<SideMenuHeader
+				eventName={event.eventName}
+				eventCode={event.eventCode}
+				startAt={event.startAt}
+				endAt={event.endAt}
+			/>
 			<SideMenuBody {...{toggleNavMenu}} />
 		</Drawer>
 	);
