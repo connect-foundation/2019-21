7e0c6fe8c58222aa646c61edb8e8553b99136c91
Yujun Kim <wnsqlehlswk@naver.com>
2019-12-09 16:40:28 +0900

    enhance: questionCard component의 불필요한 재 랜더링 막기
    # why
    QuestionCard는 react.meme 를 통해 랜더링 속도를 개선 할 수 있음

--- frontend/guest-app/src/components/Question/QuestionCard/QuestionCard.js
+++ frontend/guest-app/src/components/Question/QuestionCard/QuestionCard.js
@@ -6,20 +6,16 @@ import QuestionHeader from "./QuestionCardHeader.js";
 import QuestionBody from "./QuestionCardBody.js";
 import EmojiArea from "../../Emoji/EmojiArea.js";
 
-function QuestionCard(props) {
-	return (
-		<Card style={{margin: "0.5rem"}}>
-			<CardContent style={{paddingTop: "1rem", paddingBottom: "0"}}>
-				<QuestionHeader {...props} />
-				<Divider
-					style={{marginTop: "0.5rem", marginBottom: "0.5rem"}}
-				/>
-				<QuestionBody {...props} />
-				<EmojiArea {...props}/>
-			</CardContent>
-		</Card>
-	);
-}
+const QuestionCard = React.memo(props => (
+	<Card style={{margin: "0.5rem"}}>
+		<CardContent style={{paddingTop: "1rem", paddingBottom: "0"}}>
+			<QuestionHeader {...props} />
+			<Divider style={{marginTop: "0.5rem", marginBottom: "0.5rem"}} />
+			<QuestionBody {...props} />
+			<EmojiArea {...props} />
+		</CardContent>
+	</Card>
+));
 
 QuestionCard.proptypes = {};
 
