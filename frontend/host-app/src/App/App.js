4a707a092f76b8fb57413840a1ce17ac4af6f2ec
YujunKim <wnsqlehlswk@naver.com>
2019-12-13 00:23:34 +0900

    [doc me!!] fix: react setState callback warning 해결

--- frontend/host-app/src/App/App.js
+++ frontend/host-app/src/App/App.js
@@ -7,7 +7,6 @@ import Content from "../components/Content";
 import NewPollModal from "../components/Poll/NewPollModal";
 import {HostProvider} from "../libs/hostContext";
 import {getEventsByHost} from "../libs/gql";
-import EmptyContent from "../components/EmptyContent";
 import {socketClient} from "../libs/socket.io-Client-wrapper";
 import AppSkeleton from "../components/Skeleton/AppSkeleton.js";
 import WithApolloProvider from "../HOC/WithApolloProvider.js";
@@ -28,31 +27,22 @@ function WithEventProvider(Component) {
 
 let App = props => {
 	const {data} = props;
-	const [events, setEvents] = useState("");
+	const [events, setEvents] = useState(data.init.events);
 	const modal = false;
-	let eventNum = 0;
-
-	if (events === "") {
-		setEvents(data.init.events, () => (eventNum = data.init.events.length));
-	}
 
 	const hostInfo = data.init.host;
+	const eventId = events[0].id;
 
-	eventNum = events.length;
-	if (eventNum) {
-		const eventId = events[0].id;
-
-		socketClient.emit("joinRoom", {room: eventId});
-		socketClient.emit("event/initOption", eventId);
-	}
+	socketClient.emit("joinRoom", {room: eventId});
+	socketClient.emit("event/initOption", eventId);
 
 	return (
 		<HostProvider value={{hostInfo, events, setEvents}}>
 			<div className="App">
 				<Header />
 				<Nav />
-				{modal && <NewPollModal />}
-				{eventNum ? <Content event={event} /> : <EmptyContent />}
+				<NewPollModal open={modal} />
+				<Content event={event} />
 			</div>
 		</HostProvider>
 	);
