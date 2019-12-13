cc32a357ff58a39daf0a4404c305a22f33c13f94
hkwon <mr.hyuk.kwon@gmail.com>
2019-12-13 10:48:41 +0900

    review 요청

---
+++ review-w6/PollContainer.js
@@ -0,0 +1,189 @@
+import React, {useReducer, useState} from "react";
+import styled from "styled-components";
+import PollCard from "./PollCard";
+import {useSocket} from "../../libs/socketIoClientProvider.js";
+import reducer from "./PollReducer";
+
+const ColumnWrapper = styled.div`
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	justify-content: flex-start;
+	box-sizing: border-box;
+	border: 1px solid #dee2e6; /* Gray3 */
+	padding: 1rem;
+	width: 100%;
+`;
+
+function PollContainer({data, GuestId}) {
+	let activePollData = null;
+	let clPollData = null;
+
+	if (data) {
+		const initialPollData = data;
+
+		activePollData = initialPollData.filter(
+			poll => poll.state === "running",
+		);
+		clPollData = initialPollData.filter(poll => poll.state === "closed");
+	}
+
+	const [pollData, dispatch] = useReducer(reducer, activePollData);
+	const [closedPollData, setClosedPollData] = useState(clPollData);
+
+	const onVote = (id, candidateId, number, state) => {
+		if (state !== "running") return;
+
+		dispatch({
+			type: "VOTE",
+			id,
+			candidateId,
+			number,
+			GuestId,
+		});
+	};
+
+	const onChange = (value, state, id) => {
+		if (state !== "running") return;
+
+		dispatch({
+			type: "RATE",
+			value,
+			id,
+			GuestId,
+		});
+	};
+
+	const onCancelRating = (id, state) => {
+		if (state !== "running") return;
+
+		dispatch({
+			type: "CANCEL_RATING",
+			id,
+			GuestId,
+		});
+	};
+
+	useSocket("poll/notify_open", thePoll => {
+		if (thePoll.error) {
+			return;
+		}
+		// console.log("Guest received poll/notify_open", thePoll);
+		dispatch({
+			type: "NOTIFY_OPEN",
+			poll: thePoll,
+			GuestId,
+		});
+	});
+
+	useSocket("poll/notify_close", id => {
+		if (id.error) {
+			return;
+		}
+		// console.log("Guest received poll/notify_close", id);
+		const thePoll = pollData.filter(poll => poll.id === id)[0];
+
+		thePoll.state = "closed";
+		setClosedPollData([thePoll].concat(closedPollData));
+
+		dispatch({
+			type: "NOTIFY_CLOSE",
+			id,
+		});
+	});
+
+	useSocket("vote/on", res => {
+		if (res.error) {
+			return;
+		}
+		// 하나의 브라우저에서 여러개의 tab으로 guest들을 생성한 경우,
+		// 해당 guest를 제외한 나머지 guest에 상태가 적용되지 않아서 comment 처리했음
+		// console.log("useSocket vote/on", res);
+		// if (res.GuestId === GuestId) {
+		// 	console.log("My vote!");
+		// 	// return;
+		// }
+		dispatch({
+			type: "SOMEONE_VOTE",
+			id: res.poll.id,
+			poll: res.poll,
+			GuestId,
+		});
+	});
+
+	useSocket("vote/off", res => {
+		if (res.error) {
+			return;
+		}
+		// 하나의 브라우저에서 여러개의 tab으로 guest들을 생성한 경우,
+		// 해당 guest를 제외한 나머지 guest에 상태가 적용되지 않아서 comment 처리했음
+		// if (res.GuestId === GuestId) {
+		// 	console.log("My vote!");
+		// 	// return;
+		// }
+		dispatch({
+			type: "SOMEONE_VOTE",
+			id: res.poll.id,
+			poll: res.poll,
+			GuestId,
+		});
+	});
+
+	useSocket("rate/on", res => {
+		if (res.error) {
+			return;
+		}
+		// 하나의 브라우저에서 여러개의 tab으로 guest들을 생성한 경우,
+		// 해당 guest를 제외한 나머지 guest에 상태가 적용되지 않아서 comment 처리했음
+		// console.log("useSocket vote/on", res);
+		// if (res.GuestId === GuestId) {
+		// 	console.log("My rate!");
+		// 	// return;
+		// }
+		dispatch({
+			type: "SOMEONE_RATE",
+			id: res.poll.id,
+			poll: res.poll,
+			GuestId,
+		});
+	});
+
+	useSocket("rate/off", res => {
+		if (res.error) {
+			return;
+		}
+		// 하나의 브라우저에서 여러개의 tab으로 guest들을 생성한 경우,
+		// 해당 guest를 제외한 나머지 guest에 상태가 적용되지 않아서 comment 처리했음
+		// if (res.GuestId === GuestId) {
+		// 	console.log("My rate!");
+		// 	// return;
+		// }
+		dispatch({
+			type: "SOMEONE_RATE",
+			id: res.poll.id,
+			poll: res.poll,
+			GuestId,
+		});
+	});
+
+	return (
+		<ColumnWrapper>
+			{pollData &&
+				pollData.map(poll => (
+					<PollCard
+						{...poll}
+						key={poll.id}
+						onVote={onVote}
+						onChange={onChange}
+						onCancelRating={onCancelRating}
+					/>
+				))}
+			{closedPollData &&
+				closedPollData.map(poll => (
+					<PollCard {...poll} key={poll.id} onVote={onVote} />
+				))}
+		</ColumnWrapper>
+	);
+}
+
+export default PollContainer;
---
+++ review-w6/PollReducer.js
@@ -0,0 +1,220 @@
+import {socketClient} from "../../libs/socketIoClientProvider.js";
+
+// allowDuplication == false, 즉, 복수선택이 아닌 경우, 이전에 vote 한 candidate가 있으면 삭제해야 함
+const getCandidateToDelete = (items, candidateId) => {
+	const candidate = items.filter(
+		item => item.voted && item.id !== candidateId,
+	)[0];
+
+	return candidate ? candidate.id : null;
+};
+
+// 복수선택이 아닌 투표의 경우, 다른 선택된 항목을 uncheck 하는 함수
+const uncheckOtherItems = items => {
+	items.forEach(item => {
+		if (item.voted) {
+			item.voted = false;
+			item.voters--;
+		}
+	});
+};
+
+// N지선다형 투표에서 CLICK 으로 인해 상태 변화가 발생한 경우 처리하는 함수
+const updateItems = (items, number, allowDuplication) => {
+	const newItems = [...items];
+
+	if (newItems[number].voted) {
+		newItems[number].voted = false;
+		newItems[number].voters--;
+	} else {
+		if (!allowDuplication) {
+			uncheckOtherItems(newItems);
+		}
+		newItems[number].voted = true;
+		newItems[number].voters++;
+	}
+	return newItems;
+};
+
+// 별점 투표는 목록이 최대 별점갯수만큼 있음. 선택한 value - 1 이 index가 됨
+const updateRatingItem = (items, value, voted) => {
+	const newItems = [...items];
+
+	if (voted) {
+		newItems[value - 1].voters++;
+		newItems[value - 1].voted = voted;
+	} else {
+		newItems[value - 1].voters++;
+		newItems[value - 1].voted = voted;
+	}
+
+	return newItems;
+};
+
+// 투표의 참여 총인원수를 계산하는 함수 (복수선택 고려함)
+const updateTotalVoters = (notVoted, totalVoters, items) => {
+	let result = totalVoters;
+
+	if (notVoted) {
+		if (items.some(item => item.voted)) {
+			result = totalVoters + 1;
+		}
+	} else if (items.every(item => item.voted === false)) {
+		result = totalVoters - 1;
+	}
+
+	return result;
+};
+
+// 가장 많은 투표수를 받은 항목을 다시 계산하는 부분
+const updateFirstPlace = poll => {
+	const newPoll = {...poll};
+
+	let firstPlaceIndex = [];
+	let firstPlaceValue = 0;
+
+	newPoll.nItems.forEach((item, index) => {
+		if (item.voters == firstPlaceValue) {
+			firstPlaceIndex.push(index);
+		} else if (item.voters > firstPlaceValue) {
+			firstPlaceIndex = [];
+			firstPlaceIndex.push(index);
+			firstPlaceValue = item.voters;
+		}
+	});
+
+	// 우선 초기화
+	newPoll.nItems.forEach(item => {
+		item.firstPlace = false;
+	});
+	// 위에서 계산된 index 의 firstPlace 값만 변경함
+	firstPlaceIndex.forEach(i => {
+		newPoll.nItems[i].firstPlace = true;
+	});
+
+	console.log("firstPlace", firstPlaceIndex, newPoll.nItems);
+	return newPoll;
+};
+
+const emitVoteData = (vote, poll, candidateToDelete) => {
+	const action = poll.nItems[vote.number].voted ? "on" : "off";
+	const data = {
+		GuestId: vote.GuestId,
+		CandidateId: vote.candidateId,
+		allowDuplication: poll.allowDuplication,
+		poll,
+		candidateToDelete,
+	};
+
+	// console.log("emitVoteData", data);
+	socketClient.emit(`vote/${action}`, data);
+};
+
+const emitRateData = (rate, poll, candidateId, index) => {
+	const action = rate.type === "RATE" ? "on" : "off"; // "on" or "off"
+	const data = {
+		GuestId: rate.GuestId,
+		CandidateId: candidateId,
+		poll,
+		index,
+	};
+
+	socketClient.emit(`rate/${action}`, data);
+};
+
+export default function reducer(polls, action) {
+	let thePoll;
+	let index;
+	let candidateId;
+
+	if (action.id) {
+		thePoll = polls.filter(poll => poll.id === action.id)[0];
+	}
+
+	switch (action.type) {
+		case "NOTIFY_OPEN":
+			return [action.poll, ...polls];
+		case "NOTIFY_CLOSE":
+			return polls.filter(poll => poll.id !== action.id);
+		case "SOMEONE_VOTE":
+			thePoll.totalVoters = action.poll.totalVoters;
+			thePoll.nItems.forEach((item, index) => {
+				item.voters = action.poll.nItems[index].voters;
+				item.firstPlace = action.poll.nItems[index].firstPlace;
+			});
+			// console.log("SOMEONE_VOTE", thePoll);
+			return polls.map(poll => (poll.id === action.id ? thePoll : poll));
+		case "SOMEONE_RATE":
+			thePoll.totalVoters = action.poll.totalVoters;
+			// console.log("SOMEONE_RATE", thePoll);
+			return polls.map(poll => (poll.id === action.id ? thePoll : poll));
+		case "VOTE":
+			const notVoted = thePoll.nItems.every(item => item.voted === false);
+			let candidateToDelete = null;
+
+			if (!thePoll.allowDuplication) {
+				candidateToDelete = getCandidateToDelete(
+					thePoll.nItems,
+					action.candidateId,
+				);
+			}
+			thePoll = {
+				...thePoll,
+				nItems: updateItems(
+					thePoll.nItems,
+					action.number,
+					thePoll.allowDuplication,
+				),
+				totalVoters: updateTotalVoters(
+					notVoted,
+					thePoll.totalVoters,
+					thePoll.nItems,
+				),
+			};
+			thePoll = updateFirstPlace(thePoll);
+
+			emitVoteData(action, thePoll, candidateToDelete);
+			return polls.map(poll => (poll.id === action.id ? thePoll : poll));
+
+		case "RATE":
+			if (thePoll.rated) {
+				return polls;
+			}
+			thePoll = {
+				...thePoll,
+				nItems: updateRatingItem(thePoll.nItems, action.value, true),
+				rated: true,
+				ratingValue: action.value,
+				totalVoters: thePoll.totalVoters + 1,
+			};
+			// console.log("RATE", action, thePoll);
+			index = parseInt(action.value) - 1;
+			candidateId = thePoll.nItems[index].id;
+			emitRateData(action, thePoll, candidateId, index);
+			return polls.map(poll => (poll.id === action.id ? thePoll : poll));
+
+		case "CANCEL_RATING":
+			if (!thePoll.rated) {
+				return polls;
+			}
+			index = parseInt(thePoll.ratingValue) - 1;
+			thePoll = {
+				...thePoll,
+				nItems: updateRatingItem(
+					thePoll.nItems,
+					thePoll.ratingValue,
+					false,
+				),
+				rated: false,
+				ratingValue: 0,
+				totalVoters: thePoll.totalVoters - 1,
+			};
+			// console.log("CANCEL_RATE", action, thePoll);
+			candidateId = thePoll.nItems[index].id;
+			emitRateData(action, thePoll, candidateId, index);
+			return polls.map(poll => (poll.id === action.id ? thePoll : poll));
+
+		default:
+			throw new Error("Unhandled action.");
+	}
+}
