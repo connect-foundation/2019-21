f4058743af6a179ea4d15322ea40c3b786203201
YujunKim <wnsqlehlswk@naver.com>
2019-12-14 23:45:48 +0900

    refactoring: ModerationQuestionCard 에서 아이콘 버튼 컴포넌트 분리

--- frontend/host-app/src/components/Questions/ModerationQuestionCard.js
+++ frontend/host-app/src/components/Questions/ModerationQuestionCard.js
@@ -1,41 +1,40 @@
 import React from "react";
 import Card from "@material-ui/core/Card";
-import {CardContent, Icon} from "@material-ui/core";
-import Tooltip from "@material-ui/core/Tooltip";
-import UserAvata from "./UserAvata.js";
-import {QuestionHeader, QuestionBody, QuestionInfo, QuestionMeta, QuestionButtons} from "./QuestionStyle";
+import {CardContent} from "@material-ui/core";
+import UserAvatar from "./UserAvata.js";
+import {
+	QuestionBody,
+	QuestionButtons,
+	QuestionHeader,
+	QuestionInfo,
+	QuestionMeta,
+} from "./QuestionStyle";
 import QuestionDate from "./QuestionDate";
 import QuestionUserName from "./QuestionUserName";
 import useStyles from "./useStyles";
+import ApproveQuestionIconButton from "./ApproveQuestionIconButton.js";
+import RejectQuestionIconButton from "./RejectQuestionIconButton.js";
 
 function ModerationQuestionCard(props) {
 	const classes = useStyles();
 
 	return (
-		<Card className={props.isStared ? classes.staredQuestion : classes.normalQuestion}>
+		<Card
+			className={
+				props.isStared ? classes.staredQuestion : classes.normalQuestion
+			}
+		>
 			<CardContent>
 				<QuestionHeader>
 					<QuestionMeta>
-						<UserAvata {...props} />
+						<UserAvatar {...props} />
 						<QuestionInfo>
 							<QuestionUserName {...props} />
 							<QuestionDate {...props} />
 						</QuestionInfo>
 						<QuestionButtons>
-							<Tooltip title="승인">
-								<Icon
-									className={classes.approveButton}
-									onClick={() => props.dataHandler(props.id, props.type, "active")}>
-									check_circle_outline
-								</Icon>
-							</Tooltip>
-							<Tooltip title="거절">
-								<Icon
-									className={classes.cancelButton}
-									onClick={() => props.dataHandler(props.id, props.type, "deleted")}>
-									highlight_off
-								</Icon>
-							</Tooltip>
+							<ApproveQuestionIconButton {...props} />
+							<RejectQuestionIconButton {...props} />
 						</QuestionButtons>
 					</QuestionMeta>
 				</QuestionHeader>
---
+++ frontend/host-app/src/components/Questions/RejectQuestionIconButton.js
@@ -0,0 +1,23 @@
+import React from "react";
+import {Icon} from "@material-ui/core";
+import Tooltip from "@material-ui/core/Tooltip";
+import useStyles from "./useStyles.js";
+
+function RejectQuestionIconButton(props) {
+	const classes = useStyles();
+
+	return (
+		<Tooltip title="거절">
+			<Icon
+				className={classes.cancelButton}
+				onClick={() =>
+					props.dataHandler(props.id, props.type, "deleted")
+				}
+			>
+				highlight_off
+			</Icon>
+		</Tooltip>
+	);
+}
+
+export default RejectQuestionIconButton;
