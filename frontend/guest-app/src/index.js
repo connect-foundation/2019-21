d3b7d89b21f475c12ef3187344b7a89669eb8e9e
YujunKim <wnsqlehlswk@naver.com>
2019-12-15 00:05:29 +0900

    chore: fix typo createApolloClient.js

--- frontend/guest-app/src/index.js
+++ frontend/guest-app/src/index.js
@@ -3,7 +3,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import Cookies from "js-cookie";
 import {ApolloProvider} from "@apollo/react-hooks";
-import creaetApolloClient from "./libs/createApolloClient";
+import createApolloClient from "./libs/createApolloClient";
 import "./index.css";
 import App from "./App/App.js";
 import * as serviceWorker from "./libs/serviceWorker.js";
@@ -11,7 +11,7 @@ import config from "./config";
 
 const cookieName = "vaagle-guest";
 const token = Cookies.get(cookieName);
-const client = creaetApolloClient(config.apolloURI, token);
+const client = createApolloClient(config.apolloURI, token);
 
 
 ReactDOM.render(
