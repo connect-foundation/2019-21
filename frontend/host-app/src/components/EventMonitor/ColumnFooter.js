2d306723a4b149d943f1b884043dcdf6cdb4acb4
YujunKim <wnsqlehlswk@naver.com>
2019-12-14 19:53:11 +0900

    refactoring: ColumnFooter에 proptypes, default prop 추가

--- frontend/host-app/src/components/EventMonitor/ColumnFooter.js
+++ frontend/host-app/src/components/EventMonitor/ColumnFooter.js
@@ -1,9 +1,11 @@
 import React from "react";
 import {Icon} from "@material-ui/core";
+import PropTypes from "prop-types";
 import useStyles from "../Questions/useStyles.js";
 import ColumnFooterStyledComponent from "../StyledComponent/ColumnFooterStyledComponent.js";
 
-function ColumnFooter({data, handler}) {
+function ColumnFooter(props) {
+	const {data, handler} = props;
 	const classes = useStyles();
 	const increaseHeight = () => handler(data + 1);
 	const decreaseHeight = () => data > 0 && handler(data - 1);
@@ -29,5 +31,15 @@ function ColumnFooter({data, handler}) {
 		</ColumnFooterStyledComponent>
 	);
 }
+
+ColumnFooter.propTypes = {
+	data: PropTypes.any,
+	handler: PropTypes.func,
+};
+
+ColumnFooter.defaultProp = {
+	data: undefined,
+	handler: undefined,
+};
 
 export default ColumnFooter;
