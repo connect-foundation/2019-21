68ca1fb3c6510bc0117fdc73a8f2a22f8bb238c3
YujunKim <wnsqlehlswk@naver.com>
2019-12-14 22:21:16 +0900

    fix: ConfirmButton component fix typo onclick=> onClick

--- frontend/host-app/src/components/CommonButton/ConfirmButton.js
+++ frontend/host-app/src/components/CommonButton/ConfirmButton.js
@@ -1,13 +1,13 @@
 import {Button} from "@material-ui/core";
 import React from "react";
 
-function ConfirmButton({onclick}) {
+function ConfirmButton({onClick}) {
 	return (
 		<Button
 			size="large"
 			variant="contained"
 			color="primary"
-			onClick={onclick}
+			onClick={onClick}
 		>
 			확인
 		</Button>
