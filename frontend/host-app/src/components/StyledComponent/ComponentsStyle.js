021d9008aad169502cb8645b3601a821c221b8a6
YujunKim <wnsqlehlswk@naver.com>
2019-12-14 20:18:48 +0900

    chore: 사용되지 않는 ComponentsStyle.js 제거

--- frontend/host-app/src/components/StyledComponent/ComponentsStyle.js
+++ frontend/host-app/src/components/StyledComponent/ComponentsStyle.js
@@ -1,5 +1,27 @@
 import styled, {keyframes} from "styled-components";
 
+const TitleStyle = styled.div`
+	font-weight: bold;
+`;
+
+const RightSide = styled.div`
+	margin-left: 2rem;
+	display: flex;
+	align-items: center;
+`;
+
+const ContentStyle = styled.div`
+	display: flex;
+	flex-direction: row;
+	flex: 1;
+	overflow: auto;
+	justify-content: left;
+	align-items: flex-start;
+	padding: 4px 8px;
+	overflow-x: auto;
+	flex-wrap: nowrap;
+`;
+
 const Open = keyframes`
 	0% { 
 		min-width: 8rem; 
@@ -22,91 +44,13 @@ const Close = keyframes`
 	}
 `;
 
-const TitleBox = styled.div`
-	display: flex;
-	align-items: center;
-	width: 100%;
-	justify-content: space-around;
-	min-height: 2.5rem;
-`;
-
-const FooterBox = styled.div`
-	display: flex;
-	align-items: center;
-	width: 100%;
-	justify-content: space-around;
-	height: 1rem;
-`;
-
-const TitleStyle = styled.div`
-	font-weight: bold;
-`;
-
-const RightSide = styled.div`
-	margin-left: 2rem;
-	display: flex;
-	align-items: center;
-`;
-
-const EmptyContentBox = styled.div`
-	display: flex;
-	flex-direction: column;
-	flex: 1;
-	overflow: auto;
-	align-items: center;
-	border-radius: 8px;
-	background-color: #f1f3f5;
-	border: 1px solid #e9ecef;
-	height: 100%;
-	box-sizing: border-box;
-`;
-
-const EmptyContentDiv = styled.div`
-	display: flex;
-	align-items: center;
-	justify-content: center;
-	flex-direction: column;
-	font-size: 1.5rem;
-	margin: auto 0;
-	button {
-		margin-top: 1rem;
-	}
-`;
-
-const ContentStyle = styled.div`
-	display: flex;
-	flex-direction: row;
-	flex: 1;
-	overflow: auto;
-	justify-content: left;
-	align-items: flex-start;
-	padding: 4px 8px;
-	overflow-x: auto;
-	flex-wrap: nowrap;
-`;
-
-const QuestionStyle = styled.div`
-	display: flex;
-	flex-direction: column;
-	flex: 1;
-	justify-content: flex-start;
-	align-items: center;
-	border-radius: 8px;
-	background-color: #f1f3f5;
-	border: 1px solid #e9ecef;
-	min-width: 20rem;
-	height: ${props => props.height || "100%"};
-	box-sizing: border-box;
-	margin-left: 8px;
-`;
-
 const ModerationStyle = styled.div`
 	display: flex;
 	flex-direction: column;
 	flex: 1;
-	animation: ${props => ((props.state) ? Open : Close)};
-    animation-duration: 0.2s;
-    animation-fill-mode: forwards;
+	animation: ${props => (props.state ? Open : Close)};
+	animation-duration: 0.2s;
+	animation-fill-mode: forwards;
 	justify-content: flex-start;
 	align-items: center;
 	border-radius: 8px;
@@ -121,36 +65,9 @@ const ModerationStyle = styled.div`
 	}
 `;
 
-const SkeletonColumnStyle = styled.div`
-	display: flex;
-	flex-direction: column;
-	flex: 1;
-	justify-content: flex-start;
-	align-items: center;
-	border-radius: 8px;
-	min-width: "20rem";
-	height: "100%";
-	box-sizing: border-box;
-	& + & {
-		margin-left: 8px;
-	}
-`;
-
-const FooterStyle = styled.div`
-	width: 90%;
-	margin-top: auto;
-`;
-
 export {
 	TitleStyle,
-	TitleBox,
 	RightSide,
-	EmptyContentBox,
-	EmptyContentDiv,
 	ContentStyle,
-	QuestionStyle,
-	FooterStyle,
-	SkeletonColumnStyle,
 	ModerationStyle,
-	FooterBox,
 };
