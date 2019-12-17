04758dce3ad9bd5212b11e2c171130cf30139144
YujunKim <wnsqlehlswk@naver.com>
2019-12-17 22:38:26 +0900

    refactoring: ToggleReducer minor refactoring
    * lint 맞추기 위해 action table을 리듀서 외부로 이동
    * error 시 이전 상태를 반환하도록 수정

--- frontend/guest-app/src/components/QuestionContainer/ToggleReducer.js
+++ frontend/guest-app/src/components/QuestionContainer/ToggleReducer.js
@@ -1,41 +1,46 @@
 import _ from "lodash";
 
+const actionTable = {
+	toggle: (state, data) => {
+		const newState = _.cloneDeep(state);
+
+		newState.data = _.cloneDeep(data);
+		newState.state = !newState.state;
+
+		return newState;
+	},
+	on: (state, data) => {
+		const newState = _.cloneDeep(state);
+
+		newState.data = _.cloneDeep(data);
+		newState.state = true;
+
+		return newState;
+	},
+	off: (state, data) => {
+		const newState = _.cloneDeep(state);
+
+		newState.data = _.cloneDeep(data);
+		newState.state = false;
+
+		return newState;
+	},
+};
+
 const ToggleReducer = (state, action) => {
 	const {type, data} = action;
-
-	const actionTable = {
-		toggle: (state, data) => {
-			const newState = _.cloneDeep(state);
-
-			newState.data = _.cloneDeep(data);
-			newState.state = !newState.state;
-
-			return newState;
-		},
-		on: (state, data) => {
-			const newState = _.cloneDeep(state);
-
-			newState.data = _.cloneDeep(data);
-			newState.state = true;
-
-			return newState;
-		},
-		off: (state, data) => {
-			const newState = _.cloneDeep(state);
-
-			newState.data = _.cloneDeep(data);
-			newState.state = false;
-
-			return newState;
-		},
-	};
 
 	if (!(type in actionTable)) {
 		console.error(`unexpected action.type: ${type}`);
 		return state;
 	}
 
-	return actionTable[type](state, data);
+	try {
+		return actionTable[type](state, data);
+	} catch (e) {
+		console.error(e);
+		return state;
+	}
 };
 
 export default ToggleReducer;
