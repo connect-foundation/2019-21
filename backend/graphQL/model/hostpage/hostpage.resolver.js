94ac4c60fddde23804e4f40b948eac7a83b6828a
김근영 (노움킹김노움) <emsud@inu.ac.kr>
2019-12-17 17:08:05 +0900

    refactor: host page init query refactoring
    호스트페이지 진입 시 불러오는 init 쿼리 부분에서 jwt 값의 sub을 검사하는 부분을 함수로 분리하여 다른 리졸버 단에서도 처리가 가능하도록 수정,
    코드가 흐름에 맞게 진행되기 위해 필요한 부분은 함수로 감싸 그 의미를 명확하게 표현하고 모호한 변수명을 수정.

--- backend/graphQL/model/hostpage/hostpage.resolver.js
+++ backend/graphQL/model/hostpage/hostpage.resolver.js
@@ -11,12 +11,26 @@ import {
 	createHashtag,
 	getHashtagByEventIds,
 } from "../../../DB/queries/hashtag.js";
+import {compareCurrentDateToTarget} from "../../../libs/utils";
 
-const moderationResolver = async (eventId, moderationOption) => {
-	const updatedEvent = await updateEventById({id: eventId, moderationOption});
+function verifySubjectHostJwt(jwtSub) {
+	if (jwtSub !== "host") {
+		throw new Error("AuthenticationError");
+	}
+}
 
-	return updatedEvent[0];
-};
+function mappingHashTagsToEvents(hashTags, events, eventMap) {
+	hashTags.forEach(hashTag => {
+		const hashTagObject = hashTag.get({plain: true});
+
+		eventMap.get(hashTagObject.EventId).push(hashTagObject);
+	});
+	events.forEach(event => {
+		Object.assign(event, {HashTags: eventMap.get(event.id)});
+	});
+
+	return events;
+}
 
 const getEventOptionResolver = async eventId => {
 	const evnetOption = await getEventOptionByEventId(eventId);
@@ -27,42 +41,36 @@ const getEventOptionResolver = async eventId => {
 export default {
 	Query: {
 		init: async (_, {param}, authority) => {
-			if (authority.sub === "host") {
-				const host = authority.info;
-				let events = await getEventsByHostId(host.id);
+			verifySubjectHostJwt(authority.sub);
+			const host = authority.info;
+			let events = await getEventsByHostId(host.id);
 
-				events = events.filter(event => {
-					const eventObject = event.get({plain: true});
+			events = events.filter(event => {
+				const eventPlainObject = event.get({plain: true});
+				const diff = compareCurrentDateToTarget(eventPlainObject.endAt);
+				if (diff > 0) {
+					return eventPlainObject;
+				}
+			});
 
-					return eventObject;
-				});
+			const eventMap = new Map();
+			const eventIdList = events.map(event => {
+				eventMap.set(event.id, []);
+				return event.id;
+			});
 
-				const eventMap = new Map();
-				const eventIdList = events.map(event => {
-					eventMap.set(event.id, []);
-					return event.id;
-				});
+			const hashTags = await getHashtagByEventIds(eventIdList);
+			events = mappingHashTagsToEvents(hashTags, events, eventMap);
 
-				const hashTags = await getHashtagByEventIds(eventIdList);
-
-				hashTags.forEach(hashTag => {
-					const hashTagObject = hashTag.get({plain: true});
-
-					eventMap.get(hashTagObject.EventId).push(hashTagObject);
-				});
-				events.forEach(event => {
-					Object.assign(event, {HashTags: eventMap.get(event.id)});
-				});
-
-				return {events, host};
-			}
-
-			throw new Error("AuthenticationError");
+			return {events, host};
 		},
 		getEventOption: async (_, {EventId}) => getEventOptionResolver(EventId),
 	},
 	Mutation: {
 		createHashTags: async (_, {hashTags}, authority) => {
+			if (authority.sub !== "host") {
+				throw new Error("AuthenticationError");
+			}
 			for (const hashTag of hashTags) {
 				await createHashtag({
 					name: hashTag.name,
@@ -71,36 +79,39 @@ export default {
 			}
 		},
 		createEvent: async (_, {info}, authority) => {
-			if (authority.sub === "host") {
-				let eventCode = faker.random.alphaNumeric(4);
-				const events = await getAllEvents();
-				const existCode = events.map(event => event.eventCode);
-
-				while (true) {
-					const exist = existCode.some(
-						someCode => eventCode === someCode,
-					);
-
-					if (!exist) {
-						break;
-					}
-
-					eventCode = faker.random.alphaNumeric(4);
-				}
-				let event = await createEvent({
-					eventName: info.eventName,
-					eventCode,
-					HostId: authority.info.id,
-					startAt: info.startAt,
-					endAt: info.endAt,
-				});
-
-				event = event[0].dataValues;
-				return {...event};
+			if (authority.sub !== "host") {
+				throw new Error("AuthenticationError");
 			}
-			throw new Error("AuthenticationError");
+			let eventCode = faker.random.alphaNumeric(4);
+			const events = await getAllEvents();
+			const existCode = events.map(event => event.eventCode);
+
+			while (true) {
+				const exist = existCode.some(
+					someCode => eventCode === someCode
+				);
+
+				if (!exist) {
+					break;
+				}
+
+				eventCode = faker.random.alphaNumeric(4);
+			}
+			let event = await createEvent({
+				eventName: info.eventName,
+				eventCode,
+				HostId: authority.info.id,
+				startAt: info.startAt,
+				endAt: info.endAt,
+			});
+
+			event = event[0].dataValues;
+			return {...event};
 		},
 		updateEvent: async (_, {event}, authority) => {
+			if (authority.sub !== "host") {
+				throw new Error("AuthenticationError");
+			}
 			await updateEventById({
 				id: event.EventId,
 				eventName: event.eventName,
@@ -112,7 +123,5 @@ export default {
 
 			return updatedEvent.get({plain: true});
 		},
-		moderation: (_, {eventId, moderationOption}) =>
-			moderationResolver(eventId, moderationOption),
 	},
 };
