3e4268922ec29cc49a5a0f6eafc5ef3f19c8c72b
Yujun Kim <wnsqlehlswk@naver.com>
2019-12-18 15:41:01 +0900

    refactoring: UndoLikeModal 컴포넌트명을 LikeUndoModal로 변경

--- frontend/guest-app/src/components/LikeButton/LikeButton.js
+++ frontend/guest-app/src/components/LikeButton/LikeButton.js
@@ -2,7 +2,7 @@ import React from "react";
 import Button from "@material-ui/core/Button";
 import ThumbUpIcon from "@material-ui/icons/ThumbUp";
 import useCommonModal from "../CommonComponent/CommonModal/useCommonModal.js";
-import UndoLikeConfirmModal from "./UndoLikeModal.js";
+import UndoLikeConfirmModal from "./LikeUndoModal.js";
 import {socketClient} from "../../socket.io";
 import {useGuestGlobal} from "../../GuestGlobalProvider.js";
 
