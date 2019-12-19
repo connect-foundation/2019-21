4c6cb3ed8df5930e58bea7dbeb6ac07bf67e38d9
김근영 (노움킹김노움) <emsud@inu.ac.kr>
2019-12-19 14:10:23 +0900

    fix: token 값이 없을 때 메인으로 리다이렉팅 되도록 에러처리
    host page에서 token 값이 없을 때 error-page를 띄우던 것을 자연스럽게 메인으로 리다이렉팅 될 수 있도록 수정.

--- frontend/host-app/src/App/App.js
+++ frontend/host-app/src/App/App.js
@@ -7,6 +7,7 @@ import {HostProvider} from "../libs/hostContext";
 import {getEventsByHost} from "../libs/gql";
 import {socketClient} from "../libs/socket.io-Client-wrapper";
 import AppSkeleton from "../components/Skeleton/AppSkeleton";
+import config from "../config";
 import {compareCurrentDateToTarget} from "../libs/utils";
 
 const initialValue = "";
@@ -27,7 +28,8 @@ function App() {
 	if (loading) {
 		return <AppSkeleton />;
 	} else if (error) {
-		return <p>error-page...</p>;
+		window.location.href = config.inValidHostRedirectURL;
+		return <div />;
 	}
 	initialLoadEvents(events, initialValue, setEvents, data.init.events);
 
