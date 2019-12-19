2ea0af8c793c4e58ec17261b00d9781a63c25196
Yujun Kim <wnsqlehlswk@naver.com>
2019-12-19 14:22:03 +0900

    hotfix: fix redis client deploy setting

--- backend/redis/config/prod.config.js
+++ backend/redis/config/prod.config.js
@@ -3,8 +3,8 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const config = {
-	port: parseInt(process.env.REDIS_DEV_PORT, 10),
-	host: process.env.REDIS_DEV_HOST,
+	port: parseInt(process.env.REDIS_PROD_PORT, 10),
+	host: process.env.REDIS_PROD_HOST,
 };
 
 export default config;
