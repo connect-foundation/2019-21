9a4104d7878c9d2ccb5eac6688ceb3616d1349ba
demetoir-destop <wnsqlehlswk@naver.com>
2020-02-07 00:27:06 +0900

    chore: add todos at DB query api

--- backend/DB/queries/host.js
+++ backend/DB/queries/host.js
@@ -1,12 +1,15 @@
 import models from "../models";
 
 // todo: 더 좋은 이름
+// todo refactoring
 export async function findHostByAuthId(oauthId) {
 	const host = await models.Host.findOne({where: {oauthId}});
 
 	return host ? host.dataValues : false;
 }
 
+
+// todo refactoring
 export async function createHost(oauthId, name, image, email) {
 	const host = await models.Host.create({
 		oauthId,
