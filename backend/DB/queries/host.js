0f9298e0830c4f05ef4c79ad56628294db5e57b3
demetoir-destop <wnsqlehlswk@naver.com>
2020-02-20 14:18:14 +0900

    refactoring: host 인증시 사용되는 DB query api를 단순화시킴
    * 기존 find query 후 create query 실행하는대신 findOrCreate 로 한 쿼리에 가능하도록 수정
    * 관련 단위테스트 추가
    * jsdoc 추가

--- backend/DB/queries/host.js
+++ backend/DB/queries/host.js
@@ -11,15 +11,19 @@ export async function findHostByAuthId(oauthId) {
 	return host ? host.dataValues : false;
 }
 
-// todo refactoring
-export async function createHost(oauthId, name, image, email) {
-	const host = await Host.create({
-		oauthId,
-		name,
-		email,
-		image,
-		emailFeedBack: false,
+/**
+ *
+ * @param oauthId {string}
+ * @param name {string|undefined}
+ * @param image {string|undefined}
+ * @param email {string|undefined}
+ * @returns {Promise<object>}
+ */
+export async function findOrCreateHostByOAuth({oauthId, name, image, email}) {
+	const res = await Host.findOrCreate({
+		where: {oauthId},
+		defaults: {name, image, email, emailFeedBack: false},
 	});
 
-	return host ? host.dataValues : false;
+	return res[0].get({plain: true});
 }
--- backend/spec/DB/queries/hostQuery.test.js
+++ backend/spec/DB/queries/hostQuery.test.js
@@ -1,6 +1,6 @@
 import {before, describe, it} from "mocha";
 import assert from "assert";
-import {findHostByAuthId} from "../../../DB/queries/host.js";
+import {findOrCreateHostByOAuth} from "../../../DB/queries/host.js";
 import models from "../../../DB/models";
 
 describe("host query api", () => {
@@ -8,10 +8,22 @@ describe("host query api", () => {
 		await models.sequelize.sync();
 	});
 
-	it("be able to findHostByAuthId", async () => {
-		const authId = "1234";
-		const host = await findHostByAuthId(authId);
+	it("be able to find or create host by oauthId", async () => {
+		const oauthId = "1234";
+		const image = "image";
+		const name = "name";
+		const email = "email";
+		const host = await findOrCreateHostByOAuth({
+			oauthId,
+			image,
+			email,
+			name,
+		});
 
-		assert.deepStrictEqual(host, false);
+		assert(host.id > 0);
+		assert.equal(host.email, email);
+		assert.equal(host.name, name);
+		assert.equal(host.image, image);
+		assert.equal(host.oauthId, oauthId);
 	});
 });
